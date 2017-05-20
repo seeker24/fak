76e3a888136d630e6d92e87db9e6acd2031eb845
seeker24 <golsarchat@chmail.ir>
2017-05-20 22:05:34 +0900

    Update confing.php

--- confing.php
+++ confing.php
@@ -1,5 +1,5 @@
 <?php
 ob_start();
-define('API_KEY','توکن');
-$admin = ""; //admin user id
+define('API_KEY','356783854:AAG7mwPuq3wbAtnz9xKwArjGXvv5wGmDupQ');
+$admin = "70545327"; //admin user id
 	?>
